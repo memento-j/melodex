3424f1e15630af236b63ca08e93145d8ade17164
Julian <jsales0204@gmail.com>
2025-08-28 00:19:33 -0400

    made skeleton component responsive

--- frontend/src/components/AllPlaylistsSkeleton.tsx
+++ frontend/src/components/AllPlaylistsSkeleton.tsx
@@ -4,13 +4,13 @@ export default function AllPlaylistsSkeleton() {
   return (
     <div>
       {Array.from({ length: 3 }, (_, index) => (
-            <div key={index} className='w-140 my-8'>
+            <div key={index} className='w-110 sm:w-140 md:w-160 my-8'>
             <div className='flex items-center'>
-              <Skeleton className="size-30 object-cover rounded ml-6"/>
-              <div className="flex flex-col flex-grow">
-                <Skeleton className="ml-5 h-8 w-[240px]"/>
+              <Skeleton className="size-30 object-cover rounded ml-8 sm:ml-6"/>
+              <div className="flex flex-col flex-grow gap-5">
+                <Skeleton className="ml-8 h-8 w-40 sm:w-70 md:w-100"/>
               </div>
-                <Skeleton className="mt-2 mr-6 size-6"/>
+                <Skeleton className="mt-2 mr-11 md:mr-5 size-6"/>
             </div> 
           </div>
       ))}
